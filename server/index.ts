ac60be681d58d46175faa36a88db5008476fa653
m-taqii <busy.digital.master@gmail.com>
2026-06-29 16:10:09 +0500

    feat: initializes workers on server startup

--- server/index.ts
+++ server/index.ts
@@ -3,15 +3,16 @@ dotenv.config();
 
 import app from "./src/app";
 import { connectDB } from "./src/lib/db";
-import "./src/engine/workers"; // Initializes the global scheduler worker
+import { initializeWorkers } from "./src/engine/workers";
 
 const PORT = process.env.PORT || 3000;
 
 connectDB().then(() => {
     console.log("Connected to DB");
 
-    app.listen(PORT, () => {
+    app.listen(PORT, async () => {
         console.log(`Server running on port ${PORT}`);
+        await initializeWorkers();
     });
 }).catch((error) => {
     console.error("Failed to connect to DB", error);
